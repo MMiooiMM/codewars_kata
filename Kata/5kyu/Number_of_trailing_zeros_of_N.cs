bb9d153f1543dfd1fac18b50b98d983e5ca4a705
Mio <mio093611@gmail.com>
2019-03-02 23:25:39 +0800

    Number_of_trailing_zeros_of_N Ver 1 時間太久

---
+++ Kata/5kyu/Number_of_trailing_zeros_of_N.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kata._5kyu
+{
+    //https://www.codewars.com/kata/number-of-trailing-zeros-of-n
+    public static class Number_of_trailing_zeros_of_N
+    {
+        public static int TrailingZeros(int n)
+        {
+            return Enumerable.Range(1, n).Where(x => x % 5 == 0).Select(x => Division(x, 5)).Sum();
+        }
+        public static int Division(int number, int div)
+        {
+            if (number % div == 0)
+                return 1 + Division(number / div, div);
+            return 0;
+        }
+    }
+}
--- Kata/Program.cs
+++ Kata/Program.cs
@@ -10,8 +10,7 @@ namespace Kata
     {
         static void Main(string[] args)
         {
-            //"Program: Ladder Author: g964 Phone: +1-503-555-0090 Date: 2019-01-01 Version: 1.1"
-            var a = Matching_And_Substituting.Change("Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-009\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha", "Ladder", "1.1");
+            var a = Number_of_trailing_zeros_of_N.TrailingZeros(531);
         }
     }
 }
