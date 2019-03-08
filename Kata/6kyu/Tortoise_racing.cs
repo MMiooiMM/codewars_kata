19588ad0736ddd1f06814b2c2efcb3e42222840f
mio <mio093611@gmail.com>
2019-03-08 12:00:02 +0800

    新增 C# 版本的最佳解法

--- Kata/6kyu/Tortoise_racing.cs
+++ Kata/6kyu/Tortoise_racing.cs
@@ -9,5 +9,13 @@
             var res = g * 3600d / (v2 - v1);
             return new int[] { (int)(res / 3600), (int)(res % 3600 / 60), (int)(res % 60) };
         }
+
+        public static int[] Race_Best_Practices(int v1, int v2, int g)
+        {
+            if (v1 >= v2)
+                return null;
+            var ts = System.TimeSpan.FromSeconds((g * 3600) / (v2 - v1));
+            return new[] { ts.Hours, ts.Minutes, ts.Seconds };
+        }
     }
 }
