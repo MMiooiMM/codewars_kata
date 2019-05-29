2d970c964b919fc59838fd40319c8c35d9df8116
mio <mio093611@gmail.com>
2019-05-29 11:04:40 +0800

    日常練習 升等了~~

---
+++ Kata/4kyu/Range_Extraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata._4kyu
+{
+    //https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
+    public class Range_Extraction
+    {
+        public static string Extract(int[] args)
+        {
+            if (args is null || args.Length == 0) return "";
+            var prev = args[0];
+            var result = new List<string>();
+            var tmp = args.Skip(1).Select((e, index) => new { range = e == args[index] + 1, index }).ToList();
+            tmp.Add(new { range = false, index = args.Length - 1 });
+            foreach (var element in tmp)
+            {
+                if (!element.range)
+                {
+                    if (prev == args[element.index])
+                    {
+                        result.Add(prev + "");
+                    }
+                    else
+                    {
+                        if(prev == args[element.index - 1])
+                        {
+                            result.Add(prev + "");
+                            result.Add(args[element.index] + "");
+                        }
+                        else
+                        {
+                            result.Add(prev + "-" + args[element.index]);
+                        }                        
+                    }
+                    prev = (element.index + 1) == args.Length ? 0 : args[element.index + 1];
+                }
+            }
+            return string.Join(",", result);
+        }
+    }
+}
--- UnitTestProject/4kyu_Test.cs
+++ UnitTestProject/4kyu_Test.cs
@@ -8,6 +8,23 @@ namespace UnitTestProject
     public class _4kyu_Test
     {
         [TestMethod]
+        public void Range_Extraction_Test()
+        {
+            var actual = Range_Extraction.Extract(new[] { 1, 2 });
+            var expected = "1,2";
+            Assert.AreEqual(expected, actual);
+            actual = Range_Extraction.Extract(new[] { 1, 2, 3 });
+            expected = "1-3";
+            Assert.AreEqual(expected, actual);
+            actual = Range_Extraction.Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 });
+            expected = "-6,-3-1,3-5,7-11,14,15,17-20";
+            Assert.AreEqual(expected, actual);
+            actual = Range_Extraction.Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 });
+            expected = "-3--1,2,10,15,16,18-20";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void Next_smaller_number_with_the_same_digits_Test()
         {
             var actual = Next_smaller_number_with_the_same_digits_Clever.NextSmaller(21);
